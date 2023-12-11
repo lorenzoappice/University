5eb320734c6c0f014ef9cb7d667ecd7c7855df69
lorenzoappice <115944267+lorenzoappice@users.noreply.github.com>
2023-12-11 17:44:52 +0100

    continuo eserc 7

--- Secondo Anno/Primo semestre/Algoritmi/Codici/Lab7/lista.cpp
+++ Secondo Anno/Primo semestre/Algoritmi/Codici/Lab7/lista.cpp
@@ -12,7 +12,30 @@ int main() {
     else {
         cout << "La lista e' uguale" <<endl;
     }
-    //seconda parte
-
+    //num_elements
+    list<int> listap2;
+    for (int i; i < 10; i++) {
+        listap2.inserisciInTesta(i);
+    }
+    listap2.stampaLista();
+    int num_ele= listap2.num_elements(2,7);
+    cout << "il num di elementi da 2 a 7 sono:" << num_ele << endl;
+    //exchange
+    list<char> p1;
+    p1.inserisciInTesta('1');
+    p1.inserisciInTesta('2');
+    p1.stampaLista();
+    p1.exchange(0,1);
+    p1.stampaLista();
+    //move min e max
+    list<int> move;
+    move.inserisciInTesta(5);
+    move.inserisciInTesta(3);
+    move.inserisciInTesta(7);
+    move.inserisciInTesta(2);
+    move.inserisciInTesta(1);
+    move.stampaLista();
+    move.move_min_max();
+    move.stampaLista();
     return 0;
 }
--- Secondo Anno/Primo semestre/Algoritmi/Codici/Lab7/lista.h
+++ Secondo Anno/Primo semestre/Algoritmi/Codici/Lab7/lista.h
@@ -9,6 +9,7 @@ public:
     Nodo(T valore) : dato(valore), next(nullptr) {}
 
 };
+
 template <typename T>
 class list {
 private:
@@ -73,5 +74,98 @@ public:
         }
         // Se una lista è più lunga dell'altra, sono diverse
         return corrente1 != corrente2;
+    }
+
+    Nodo<T>* getNodeAtIndex(int index) const {
+        Nodo<T>* current = testa;
+
+        for (int i = 0; i < index && current; ++i) {
+            current = current->next;
+        }
+        return current;
+    }
+
+    void clear() {
+
+
+    }
+    int num_elements(int pi,int pf) const {
+        Nodo<T>* current = getNodeAtIndex(pi);
+        int num_elements=0;
+        for(int i = pi; i<pf && current; ++i) {
+            current = current -> next ;
+            num_elements++;
+        }
+        return num_elements+1;
+    }
+    void exchange(int p1,int p2) {
+        Nodo<T>* elemp1 = getNodeAtIndex(p1);
+        Nodo<T>* elemp2 = getNodeAtIndex(p2);
+        T elemento1 = elemp1->dato;
+        T elemento2 = elemp2->dato;
+        elemp1->dato = elemento2;
+        elemp2->dato = elemento1;
+    }
+    void move_min_max() {
+        T min = find_min();
+        T max = find_max();
+        Nodo<T>* corrente = testa;
+        Nodo<T>* precedente = nullptr;
+        Nodo<T>* minNode = nullptr;
+        Nodo<T>* maxNode = nullptr;
+
+        while (corrente) {
+            if (corrente->dato == min) {
+                minNode = corrente;
+            } else if (corrente->dato == max) {
+                maxNode = corrente;
+            }
+            corrente = corrente->next;
+        }
+        // Spostamento del minimo all'inizio
+        if (minNode && minNode != testa) {
+            corrente = testa;
+            while (corrente->next != minNode) {
+                corrente = corrente->next;
+            }
+            corrente->next = minNode->next;
+            minNode->next = testa;
+            testa = minNode;
+        }
+        // Spostamento del massimo alla fine
+        if (maxNode) {
+            corrente = testa;
+            while (corrente->next) {
+                corrente = corrente->next;
+            }
+            if (maxNode != corrente) {
+                precedente->next = maxNode->next;
+                corrente->next = maxNode;
+                maxNode->next = nullptr;
+            }
+        }
+    }
+    T find_min() const {
+        Nodo<T>* corrente = testa;
+        T min = corrente->dato;
+        while (corrente) {
+            if (corrente->dato >= min) {
+                min = corrente->dato;
+            }
+            corrente = corrente->next;
+        }
+        return min;
+    }
+
+    T find_max() const {
+        Nodo<T>* corrente = testa;
+        T max= corrente->dato;
+        while (corrente) {
+            if (corrente->dato <= max) {
+                max = corrente->dato;
+            }
+            corrente = corrente->next;
+        }
+        return max;
     }
 };
